e2778d136f77561239acbb6e3403464d4b354223
andyandnat <149843636+andyandnat@users.noreply.github.com>
2025-05-01 11:22:34 -0600

    cart changes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,3 @@
-
-
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -27,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -71,12 +69,17 @@ document.querySelectorAll('.js-add-to-cart')
         }
       });
 
+      const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`
+      );
+      const quantity = Number(quantitySelector.value);
+
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
       } else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: quantity
         });
       }
 
@@ -85,6 +88,7 @@ document.querySelectorAll('.js-add-to-cart')
       cart.forEach((item) => {
         cartQuantity += item.quantity;
       });
+      console.log(cart);
 
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
