faa486a5fa4356b1337904e44e8c44d6eb112e07
andyandnat <149843636+andyandnat@users.noreply.github.com>
2025-05-01 16:42:26 -0600

    New products added

--- data/products.js
+++ data/products.js
@@ -656,5 +656,25 @@ const products = [
       "apparel",
       "mens"
     ]
-  }
+  },
+  {
+    id: "id1",
+    image: "images/products/backpack.jpg",
+    name: "Backpack",
+    rating: {
+      stars: 4.5,
+      count: 200
+    },
+    priceCents: 2500,
+  },
+  {
+    id: "id2",
+    image: "images/products/umbrella.jpg",
+    name: "Umbrella",
+    rating: {
+      stars: 5,
+      count: 456
+    },
+    priceCents: 2999,
+  },
 ];
